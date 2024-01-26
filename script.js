69baf8a30b3a05d82a3a4bb2aeccb01051b49d08
oddphie <65217607+Oddphie@users.noreply.github.com>
2024-01-26 22:05:20 +1000

    Add about me section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const main = document.getElementsByTagName("main")[0];
 const sidebar = document.getElementById("sidebar");
 const content = document.getElementById("content");
 
+// Staggered reveal sidebar effect
 function revealSidebar() {
 	for (let i = 0; i < sidebar.children.length; i++) {
 		sidebar.children[i].style.animationDelay = `calc(500ms + ${i * 150}ms)`;
