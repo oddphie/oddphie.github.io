20a79cf26d480fb2245345146ec62bf68abb44bd
oddphie <65217607+Oddphie@users.noreply.github.com>
2024-02-02 15:05:09 +1000

    Add about me minor details and homepage title

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ const content = document.getElementById("content");
 // Staggered reveal sidebar effect
 function revealSidebar() {
 	for (let i = 0; i < sidebar.children.length; i++) {
+		// Add animation delay effect to each sidebar element
 		sidebar.children[i].style.animationDelay = `calc(500ms + ${i * 150}ms)`;
 	}
 }
 
 revealSidebar();
+
+// Staggered reveal title words effect
+function revealTitle() {
+	let titles = document.querySelectorAll(".title");
+	
+	titles.forEach((title) => {
+		let words = title.textContent.split(" ");
+		title.innerText = "";
+
+		words.forEach((word) => {
+			// Append each word to span
+			let span = document.createElement("span");
+			span.append(word + " ");
+			title.appendChild(span);
+		});
+
+		for (let i = 0; i < title.children.length; i++) {
+			// Add animation delay effect to each span
+			title.children[i].style.animationDelay = `calc(1350ms + ${i * 150}ms)`;
+			
+		}
+	});
+}
+
+revealTitle();
