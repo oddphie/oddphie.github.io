831da41b267c265f4276409409b3daf59e74554d
oddphie <65217607+Oddphie@users.noreply.github.com>
2024-10-18 20:38:05 +1000

    Update nav bar to script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+"use strict";
+
+// Create skill bars
 const skills = document.getElementById("skills");
 
 skills.childNodes.forEach((skill) => {
@@ -11,22 +14,69 @@ skills.childNodes.forEach((skill) => {
 	}
 });
 
-const sections = [document.getElementById("me-section"), document.getElementById("my-stuff-section"), document.getElementById("contact-section")];
+const nav = document.getElementById("nav-bar");
+let currentSection = "me";
 
-for (let i = 0; i < sections.length; i++) {
-	if (i != 0) {
-		sections[i].style.display = "none";
-	}
-}
+function createNav() {
+	const list = document.createElement("ul");
+	nav.appendChild(list);
 
-const nav = document.querySelectorAll("nav>ul>li>a");
+	const nav_items = [
+		["hero", "", ["mdi:star-four-points-outline", "mdi:star-four-points"]],
+		["me", "Me", ["mingcute:user-3-line", "mingcute:user-3-fill"]],
+		["my-stuff", "My stuff", ["mingcute:box-3-line", "mingcute:box-3-fill"]],
+		["contact", "Contact", ["mingcute:send-plane-line", "mingcute:send-plane-fill"]],
+	];
 
-for (let i = 0; i < nav.length; i++) {
-	nav[i].onclick = () => {
-		for (let j = 0; j < sections.length; j++) {
-            console.log(i, j, i == j)
-			if (i == j) sections[j].style.display = "";
-            else sections[j].style.display = "none";
+	nav_items.forEach((item) => {
+		let section = document.getElementById(item[0]);
+
+		let list_item = document.createElement("li");
+		list.appendChild(list_item);
+
+		let link = document.createElement("a");
+		link.href = "#" + item[0];
+		link.innerText = item[1];
+		list_item.appendChild(link);
+
+		let icon = document.createElement("iconify-icon");
+		icon.icon = item[2][0];
+		link.appendChild(icon);
+
+		if (item[0] === "hero" || item[0] === currentSection) {
+			section.style.display = "";
+		} else {
+			section.style.display = "none";
 		}
-	};
+
+		link.onclick = () => {
+			if (item[0] != currentSection) {
+				hideCurrentSection(nav_items);
+
+				currentSection = item[0];
+				section.style.display = "";
+				icon.icon = item[2][1];
+			} else {
+				icon.icon = item[2][1];
+			}
+		};
+	});
 }
+
+function hideCurrentSection(nav_items) {
+	// Get the nav item array of the current section
+	nav_items.forEach((item) => {
+		if (item.includes(currentSection)) {
+			// Set icon to line
+			document.querySelector(`a[href='#${currentSection}']>iconify-icon`).icon = item[2][0];
+
+			// Don't hide to hero section
+			if (currentSection !== "hero") {
+				// Hide section
+				document.getElementById(item[0]).style.display = "none";
+			}
+		}
+	});
+}
+
+createNav();
